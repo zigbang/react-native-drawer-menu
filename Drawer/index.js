1c62daede70148d705920c454adfbe4582f1477e
owen <owen@zigbang.com>
2018-12-11 12:48:09 +0900

    layer 포지션 값 조정

--- Drawer/index.js
+++ Drawer/index.js
@@ -428,7 +428,7 @@ const styles = StyleSheet.create({
     flex: 1
   },
   absolute: {
-    position: 'absolute'
+    position: 'absolute',top:0, bottom:0, left:0, right:0
   },
   mask: {
     left: 0,
